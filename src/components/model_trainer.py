fcaa7fe930e168565318d4d8550d9e6b30a24c27
ayushzoc <ayush168662@gmail.com>
2023-04-04 18:39:52 -0500

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,11 +49,46 @@ class ModelTrainer:
                 'AdaBoost Regressor': AdaBoostRegressor()
             }
 
+            parameters = {
+                "Decision Tree": {
+                "criterion": ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                "max_features": ['sqrt', 'log2']
+                },
+                "Random Forest": {
+                "criterion": ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                "max_features": ['sqrt', 'log2'],
+                "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                "learning_rate": [.1, .01, .05, .001],
+                "subsample": [0.5, 0.6, 0.7, 0.8, 0.85, 0.9],
+                "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Classifier": {
+                "n_neighbors": [5, 7, 9, 11]
+                },
+                "XGBRegressor": {
+                "learning_rate": [.1, .01, .001, .05],
+                "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                "depth": [6, 8, 10],
+                "learning_rate": [.01, .1, .05],
+                "iterations": [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                "learning_rate": [.1, .01, .05, .001],
+                "n_estimators": [8, 16, 32, 64, 128, 256]
+                }
+            }
+
             model_report:dict = evaluate_models(X_train = X_train,
                                                y_train = y_train,
                                                X_test = X_test,
                                                y_test = y_test,
-                                               models = models)
+                                               models = models,
+                                               params = parameters)
             
             # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
